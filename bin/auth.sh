bdfa6ae2e150fbd62912dc08af586f23df064ff7
silver <silv3rr@users.noreply.github.com>
2024-08-09 22:09:35 +0200

    auth.sh: add auth_pages.conf

--- bin/auth.sh
+++ bin/auth.sh
@@ -1,51 +1,72 @@
-#!/bin/sh -x
+#!/bin/sh
 
-echo "Setting up webgui auth..."
+# glftpd-webui auth setup
+
+# shellcheck disable=SC2016
+default_htpasswd='shit:$apr1$8kedvKJ7$PuY2hy.QQh6iLP3Ckwm740'
+modes="basic|glftpd|both|none"
 
 rm -rf /etc/nginx/http.d/auth-server.conf
 rm -rf /etc/nginx/auth.d/*.conf
 cp -rf /etc/nginx/auth.d/allow_conf.template /etc/nginx/auth.d/allow.conf
 
-func_setuser() {
-  if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
-    sed -i "s|^\(.*'http_auth'\s*=>\).*$|\1 \['username\' => '$USERNAME', 'password' => '$PASSWORD'\]|" /app/config.php;
-    echo "$PASSWORD" | htpasswd -n -i "$USERNAME" > /etc/nginx/.htpasswd
-  fi
-}
+if [ -n "$WEBUI_AUTH_USER" ]; then
+  USERNAME="$WEBUI_AUTH_USER"
+fi
+if [ -n "$WEBUI_AUTH_PASS" ]; then
+  PASSWORD="$WEBUI_AUTH_PASS"
+fi
 
 if [ -z "$AUTH" ] && [ -n "$1" ]; then
   AUTH="$1"
 fi
+if [ -z "$USERNAME" ] && [ -n "$2" ]; then
+  USERNAME="$2"
+fi
+if [ -z "$PASSWORD" ] && [ -n "$3" ]; then
+  PASSWORD="$3"
+fi
 
-if echo "$AUTH" | grep -Eq "^(basic|glftpd|both|none)$"; then
+if echo "$AUTH" | grep -Eq "^($modes)$"; then
+  echo "Setting up webgui auth..."
   case $AUTH in
     basic)
-      func_setuser
+      if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
+        echo "$PASSWORD" | htpasswd -n -i "$USERNAME" > /etc/nginx/.htpasswd
+        sed -i "s|^\(.*'http_auth'\s*=>\).*$|\1 \['username\' => '$USERNAME', 'password' => '$PASSWORD'\],|" /app/config.php;
+      else
+        echo "$default_htpasswd" > /etc/nginx/.htpasswd
+      fi
       cp -rf /etc/nginx/http.d/auth-server.conf.template /etc/nginx/http.d/auth-server.conf
+      cp -rf /etc/nginx/auth.d/auth_pages.conf.template /etc/nginx/auth.d/auth_pages.conf
       cp -rf /etc/nginx/auth.d/auth_basic.conf.template /etc/nginx/auth.d/auth_basic.conf
-      sed -i "/auth_request/d" /etc/nginx/http.d/webui.conf
-      sed -i "/error_page 401 = @auth_401/d" /etc/nginx/http.d/webui.conf
     ;;
     glftpd)
       cp -rf /etc/nginx/http.d/auth-server.conf.template /etc/nginx/http.d/auth-server.conf
+      cp -rf /etc/nginx/auth.d/auth_pages.conf.template /etc/nginx/auth.d/auth_pages.conf
       cp -rf /etc/nginx/auth.d/auth_request.conf.template /etc/nginx/auth.d/auth_request.conf
-      sed -i "/auth_basic/d" /etc/nginx/http.d/webui.conf
       echo "auth_basic off;" > /etc/nginx/auth.d/auth_basic.conf
     ;;
     both)
-      func_setuser
+      if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
+        sed -i "s|^\(.*'http_auth'\s*=>\).*$|\1 \['username\' => '$USERNAME', 'password' => '$PASSWORD'\],|" /app/config.php;
+      fi
+      rm -rf /etc/nginx/.htpasswd
       rm -rf /etc/nginx/auth.d/auth_basic.conf
       cp -rf /etc/nginx/http.d/auth-server.conf.template /etc/nginx/http.d/auth-server.conf
+      cp -rf /etc/nginx/auth.d/auth_pages.conf.template /etc/nginx/auth.d/auth_pages.conf
       cp -rf /etc/nginx/auth.d/auth_request.conf.template /etc/nginx/auth.d/auth_request.conf
     ;;
     none)
       rm -rf /etc/nginx/http.d/auth-server.conf
+      rm -rf /etc/nginx/auth.d/auth_pages.conf
       rm -rf /etc/nginx/auth.d/auth_basic.conf
       rm -rf /etc/nginx/auth.d/auth_request.conf
       echo "auth_basic off;" > /etc/nginx/auth.d/auth_basic.conf
-      sed -i -r "s/(auth_basic|auth_request)/d" /etc/nginx/http.d/webui.conf
     ;;
   esac
-  sed -i -r "s/^(.*'auth'\s*=>\s*\")(basic|glftpd|both|none|)(\",.*)$/\1$AUTH\3/" /app/config.php
+  sed -i -r "s/^(.*'auth'\s*=>\s*\")($modes|)(\",.*)$/\1$AUTH\3/" /app/config.php
   /usr/sbin/nginx -s reload
+else
+  echo "$0 <$modes> [username] [password]"
 fi
