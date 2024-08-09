4e429d4ee64f3639db59b1327fceb51f6f9add44
silver <silv3rr@users.noreply.github.com>
2024-08-09 22:13:56 +0200

    entrypoint.sh: rename WEBUI vars

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -7,7 +7,7 @@ echo "Looking up glftpd host... "
 { nslookup -type=a glftpd 127.0.0.11 | grep -A 99 answer:; } | grep -q Address || echo "127.0.0.1 glftpd" >>/etc/hosts
 cp -rf /etc/nginx/http.d/webui.conf.template /etc/nginx/http.d/webui.conf
 sed -i "s/\( *listen\) .* ssl;$/\1 ${WEBUI_PORT:-443} ssl;/" /etc/nginx/http.d/webui.conf
-test -x /auth.sh && USERNAME="$WEBUI_USERNAME" PASSWORD="$WEBUI_PASSWORD" AUTH="$WEBUI_AUTHMODE" /auth.sh
+test -x /auth.sh && USERNAME="$WEBUI_AUTH_USER" PASSWORD="$WEBUI_AUTH_PASS" AUTH="$WEBUI_AUTH_MODE" /auth.sh
 chown 65534:root /app/config.php
 $(echo /usr/sbin/php-fpm*) -F &
 nginx -g "daemon off;"
