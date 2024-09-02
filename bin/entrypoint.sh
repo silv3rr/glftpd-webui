c4c67ca9a6733ad46968a643a34f653c90320588
silver <silv3rr@users.noreply.github.com>
2024-09-02 17:49:47 +0200

    Dockerfile: use config.php.template

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,13 +1,21 @@
-#!/bin/sh -x
+#!/bin/sh
 
 DOCKER_GID="$(stat -c %g /var/run/docker.sock 2>/dev/null)"
 grep -Eq "^docker:" /etc/group || echo "docker:x:${DOCKER_GID:-999}:nobody" >>/etc/group
+
 echo "Looking up glftpd host... "
-{ nslookup -type=a glftpd-web 127.0.0.11 | grep -A 99 answer:; } | grep -q Address || echo "127.0.0.1 glftpd-web" >>/etc/hosts
-{ nslookup -type=a glftpd 127.0.0.11 | grep -A 99 answer:; } | grep -q Address || echo "127.0.0.1 glftpd" >>/etc/hosts
-cp -rf /etc/nginx/http.d/webui.conf.template /etc/nginx/http.d/webui.conf
-sed -i "s/\( *listen\) .* ssl;$/\1 ${WEBUI_PORT:-443} ssl;/" /etc/nginx/http.d/webui.conf
-test -x /auth.sh && USERNAME="$WEBUI_AUTH_USER" PASSWORD="$WEBUI_AUTH_PASS" AUTH="$WEBUI_AUTH_MODE" /auth.sh
+{ nslookup -type=a glftpd-web 127.0.0.11 | grep -A 99 answer:; } | grep -q Address || \
+    echo "127.0.0.1 glftpd-web" | tee -a /etc/hosts
+echo "Looking up web host... "
+{ nslookup -type=a glftpd 127.0.0.11 | grep -A 99 answer:; } | grep -q Address || \
+    echo "127.0.0.1 glftpd" | tee -a /etc/hosts
+
+test -s /app/config.php || cp -f /app/config.php.dist /app/config.php
 chown 65534:root /app/config.php
+
+cp -f /etc/nginx/http.d/webui.conf.template /etc/nginx/http.d/webui.conf
+sed -i "s/\( *listen\) .* ssl;$/\1 ${WEBUI_PORT:-443} ssl;/" /etc/nginx/http.d/webui.conf
+test -x /auth.sh && /auth.sh
+
 $(echo /usr/sbin/php-fpm*) -F &
 nginx -g "daemon off;"
