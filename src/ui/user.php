62e97eeaf43c9385386cac8a3fe04c1d10325f8a
silver <silv3rr@users.noreply.github.com>
2024-12-06 16:27:12 +0100

    src: user.php, assets: css - improve showing spy userinfo
    update: etc/nginx/http.d/webui.conf.template

--- src/ui/user.php
+++ src/ui/user.php
@@ -1,13 +1,22 @@
 <?php
 
-// stub; nginx redirects pyspy's /user/<username> here
+// nginx redirects pyspy's /user/<username> here
 
 session_start();
 
 header('Content-Type: application/json');
 
 if (!empty($_GET["user"])) {
-    print json_encode($_SESSION['userfile']);
+    print json_encode(
+        array(
+            'LINK' => "<a href=/index.php?user={$_GET['user']}>CHANGE USER</a>",
+            'LOGINS' => $_SESSION['userfile']['LOGINS'],
+            'FLAGS' => $_SESSION['userfile']['FLAGS'],
+            'IP' => $_SESSION['userfile']['IP'],
+            'RATIO' => $_SESSION['userfile']['RATIO'],
+            'CREDITS' => $_SESSION['userfile']['CREDITS'],
+        )
+    );
 } else {
     print json_encode(['user' => 'not found']);
 }
